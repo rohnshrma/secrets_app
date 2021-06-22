95085f3314d8b29fdc40a7932c094b8ad48f9509
rohnshrma <rohn11shrma@gmail.com>
2021-06-22 12:26:25 +0530

    another commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -9,12 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-// mongoose
 mongoose.connect(
-  "mongodb+srv://Admin-Rohan:"+ process.env.MONGO_ADMIN_PASS +"@cluster0.2vxj1.mongodb.net/userDB",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-
+    "mongodb+srv://Admin-Rohan:"+ process.env.ADMIN_PASS +"@cluster0.2vxj1.mongodb.net/userDB",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -27,7 +25,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ['password']})
+userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ['password']});
 
 
 // ṃodel
@@ -42,36 +40,34 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
   const user = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  user.save((err) => {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
+    email:req.body.username,
+    password:req.body.password
+  })
+  user.save((err)=>{
+    if(!err){
+    res.render("secrets")      
     }
-  });
+  })
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 app.post("/login", (req, res) => {
-  const userName = req.body.username;
-  const userPassword = req.body.password;
-
-  User.findOne({ email: userName }, (err, foundUser) => {
-    if (!err) {
-      if (foundUser) {
-        if (foundUser.password === userPassword) {
-          res.render("secrets");
-        }
-      }
-    } else {
-      console.log(err);
+const userName = req.body.username;
+const userpass = req.body.password;
+User.findOne({email:userName},(err,foundUser)=>{
+if(!err){
+  if(foundUser){
+    if(foundUser.password === userpass){
+      res.render("secrets")
     }
-  });
+  }
+}else{
+  console.log(err);
+}
+})
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
